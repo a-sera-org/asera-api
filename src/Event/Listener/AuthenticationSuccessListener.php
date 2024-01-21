bda39eecc483fdf0b40997256f8791d490a91a67
julkwel <jrajerison@bocasay.com>
2024-01-21 16:21:48 +0300

    Add supplement data for token

---
+++ src/Event/Listener/AuthenticationSuccessListener.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @author julienrajerison5@gmail.com jul
+ *
+ * Date : 21/01/2024
+ */
+
+namespace App\Event\Listener;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AuthenticationSuccessListener
+{
+    /**
+     * Add supplement data.
+     *
+     * @param AuthenticationSuccessEvent $event
+     *
+     * @return void
+     */
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event): void
+    {
+        $data = $event->getData();
+        $user = $event->getUser();
+
+        /** @var $user User|UserInterface */
+        if (!$user instanceof UserInterface) {
+            return;
+        }
+
+        $data['data'] = [
+            'username' => $user->getusername(),
+            'roles' => $user->getRoles(),
+            'sex' => $user->getSex(),
+            'contact' => $user->getContact(),
+            'profile_picture' => $user->getMedia() ? $user->getMedia()->getProfilePicture() : '',
+            'couverture_image' => $user->getMedia() ? $user->getMedia()->getCoverPicture() : '',
+            'firstname' => $user->getFirstname(),
+            'lastname' => $user->getLastname(),
+        ];
+
+        $event->setData($data);
+    }
+}
