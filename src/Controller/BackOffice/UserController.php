61dd49dfe39d30b33ba59e14884649f712b85f36
njaina <hei.njaina.2@gmail.com>
2024-02-28 00:32:32 +0300

    up

--- src/Controller/BackOffice/UserController.php
+++ src/Controller/BackOffice/UserController.php
@@ -16,7 +16,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/admin/users', name: 'admin_user_')]
+    #[Route('/admin/users', name: 'admin_user_')]
 class UserController extends AbstractController
 {
     public function __construct(private UserHandler $userHandler, private UserRepository $userRepository, private Paginator $paginator)
