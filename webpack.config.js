ca4ba3eedde5d48c329b8c2d0ec2121a894bab79
Francesca001 <franciscaramialisoa@gmail.com>
2024-02-27 13:44:56 +0100

    add entreprise description ckeditor

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addEntry('bo_entry', './assets/bo_entry.js')
+    .addEntry('ckeditor', './assets/ckeditor.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
