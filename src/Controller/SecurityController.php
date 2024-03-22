16e6b4920d336b505d4cae7e834f8f232ec062e6
njaina <hei.njaina.2@gmail.com>
2024-03-22 11:43:25 +0300

    feat: udpate security controller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,29 +10,29 @@ use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mailer\Transport;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Util\TokenGenerator;
 
 class SecurityController extends AbstractController
 {
   private $userRepository;
   private $entityManager;
   private $passwordHasher;
-  private $mailer;
 
   public function __construct(
     UserRepository $userRepository,
     EntityManagerInterface $entityManager,
     UserPasswordHasherInterface $passwordHasher,
-    MailerInterface $mailer
   ) {
     $this->userRepository = $userRepository;
     $this->entityManager = $entityManager;
     $this->passwordHasher = $passwordHasher;
-    $this->mailer = $mailer;
   }
 
   #[Route(path: '/login', name: 'app_login')]
@@ -53,63 +53,67 @@ class SecurityController extends AbstractController
   {
     throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
   }
-  #[Route('/reset-password', name: 'reset_password_route')]
-  public function resetPassword(Request $request, SessionInterface $session, UrlGeneratorInterface $urlGenerator): Response
+
+  #[Route('/reset-password', name: 'reset_password')]
+  public function resetPassword(Request $request, SessionInterface $session, UrlGeneratorInterface $urlGenerator, UserRepository $userRepository, MailerInterface $mailer): Response
   {
-    $email = $request->request->get('email');
-    dump($email);
-    $email = trim($email);
-    $user = $this->userRepository->findByEmail($email);
+    if ($request->isMethod('POST')) {
+      $email = $request->request->get('email');
 
-    if ($user) {
-      // Generate a token for password reset
-      $token = Uuid::v4();
-      // Store the token in the session
-      $session->set('reset_token', $token);
+      if ($email) {
+        $email = trim($email);
+        $user = $userRepository->findByEmail($email);
 
-      // Send password reset email
-      $resetPasswordUrl = $urlGenerator->generate('reset_password_confirm', ['token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
-      $email = (new Email())
-      ->from('hei.njaina.2@gmail.com')
-      ->to($email)
-      ->subject('Password Reset')
-      ->html($this->renderView('email/reset_password.html.twig', [
-        'resetPasswordUrl' => $resetPasswordUrl,
-      ]));
+        if ($user) {
+          // Générer un token pour la réinitialisation du mot de passe
+          $token = TokenGenerator::generateSixDigitToken();
+          // Stocker le token en session
+          $session->set('reset_token', $token);
 
-      $this->mailer->send($email);
+          // Envoyer l'e-mail de réinitialisation du mot de passe
+          $resetPasswordUrl = $urlGenerator->generate('reset_password_confirm', ['token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
+          $email = (new Email())
+          ->from('noreply@example.com')
+          ->to($email)
+          ->subject('Réinitialisation du mot de passe')
+          ->html($this->renderView('/security/email/reset_password.html.twig', [
+            'resetPasswordUrl' => $resetPasswordUrl,
+            'token' => $token,
+          ]));
 
-      $this->addFlash('success', 'Password reset link has been sent to your email.');
+          $transport = Transport::fromDsn($_ENV['MAILER_DSN']);
+          $mailer = new Mailer($transport);
+          $mailer->send($email);
 
-      return $this->redirectToRoute('password_reset_requested');
+          $this->addFlash('success', 'Un lien de réinitialisation du mot de passe a été envoyé à votre adresse e-mail.');
+
+          return $this->redirectToRoute('reset_password_confirm', ['token' => $token]);
+        } else {
+          $this->addFlash('error', 'Adresse e-mail introuvable.');
+        }
+      } else {
+        $this->addFlash('error', 'Le champ d\'e-mail est requis.');
+      }
     }
 
-    $this->addFlash('error', 'Email not found.');
-
-    return $this->redirectToRoute('reset_password');
+    return $this->render('security/reset_password.html.twig');
   }
+  #[Route('/reset-password/confirm', name: 'reset_password_confirm')]
+    public function confirmResetPassword(Request $request, SessionInterface $session): Response
+    {
+        if ($request->isMethod('POST')) {
+            $tokenFromRequest = $request->request->get('token');
+            $resetToken = $session->get('reset_token');
 
-  #[Route('/reset-password/requested', name: 'password_reset_requested')]
-  public function passwordResetRequested(): Response
-  {
-    return $this->render('password_reset_requested.html.twig');
-  }
+            if ($tokenFromRequest && $tokenFromRequest === $resetToken) {
+                // Implémentez ici la logique pour gérer la confirmation de la réinitialisation du mot de passe
+                return $this->redirectToRoute('reset_password_form');
+            } else {
+                throw new \InvalidArgumentException('Invalid token');
+            }
+        }
 
-  #[Route('/reset-password/confirm/{token}', name: 'reset_password_confirm')]
-  public function confirmResetPassword($token, SessionInterface $session): Response
-  {
-    // Retrieve token from session
-    $resetToken = $session->get('reset_token');
-
-    // Check if token matches
-    if ($token !== $resetToken) {
-      throw new \InvalidArgumentException('Invalid token');
+        return $this->render('security/reset_password_confirm.html.twig');
     }
-
-    // Implement logic to handle password reset confirmation
-
-    return $this->redirectToRoute('reset_password_form');
-  }
-
-
 }
+
